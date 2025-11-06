e198901182ace3db36fd230ea2b200ed2bc7a630
Khouloud <zeghlachekhouloudassia@gmail.com>
2025-11-06 11:23:54 +0100

    la precedente version est fausse .this is the correct one

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -7,27 +7,21 @@ import org.emp.gl.clients.Horloge ;
 import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
 import org.emp.gl.timer.service.TimerService;
 
-/**
- * Hello world!
- *
- */
 public class App {
 
     public static void main(String[] args) {
 
         testDuTimeService();
-       // TimeService timer= new DummyTimerService();
-       // Horloge h1 =new Horloge (timer);
-       // timer.start(); 
+    
     }
 
     private static void testDuTimeService() {
          
          TimerService timer = new DummyTimeServiceImpl();
     
-       // Horloge h1 = new Horloge("Num 1", timer);
-        //Horloge h2 = new Horloge("Num 2", timer);
-        //Horloge h3 = new Horloge("Num3", timer);
+        Horloge h1 = new Horloge("Num 1", timer);
+        Horloge h2 = new Horloge("Num 2", timer);
+        Horloge h3 = new Horloge("Num3", timer);
         
         new CompteARebours("C1", 5, timer);
 
@@ -38,8 +32,10 @@ public class App {
             int j=i+10;
           new CompteARebours("C" + i, j, timer);
         }
+    }
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
+
 }
