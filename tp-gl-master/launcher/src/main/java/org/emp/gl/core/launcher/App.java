cc316dc94f59ab8f838a28acacd5da14eef852ce
Khouloud <zeghlachekhouloudassia@gmail.com>
2025-11-06 10:43:11 +0100

    reponses A/B/C

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -1,6 +1,8 @@
 package org.emp.gl.core.launcher;
 
 import org.emp.gl.clients.Horloge ;
+import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
+import org.emp.gl.timer.service.TimerService;
 
 /**
  * Hello world!
@@ -11,10 +13,21 @@ public class App {
     public static void main(String[] args) {
 
         testDuTimeService();
+       // TimeService timer= new DummyTimerService();
+       // Horloge h1 =new Horloge (timer);
+       // timer.start(); 
     }
 
     private static void testDuTimeService() {
-        Horloge horloge = new Horloge("Num 1") ;
+         
+         TimerService timer = new DummyTimeServiceImpl();
+    
+        Horloge h1 = new Horloge("Num 1", timer);
+        Horloge h2 = new Horloge("Num 2", timer);
+        Horloge h3 = new Horloge("Num3", timer);
+
+        
+    
     }
 
     public static void clearScreen() {
--- tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
+++ tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
@@ -1,18 +1,29 @@
 package org.emp.gl.clients ; 
 
+import org.emp.gl.timer.service.TimerChangeListener;
 import org.emp.gl.timer.service.TimerService ; 
 
 
-public class Horloge {
+public class Horloge implements TimerChangeListener {
 
     String name; 
     TimerService timerService ; 
+     
+   
 
-
-    public Horloge (String name) {
+    public Horloge (String name,TimerService timerService) {
         this.name = name ; 
-
+        this.timerService =timerService;
+        timerService.addTimeChangeListener(this);
         System.out.println ("Horloge "+name+" initialized!") ;
+    }
+    @Override
+    public void propertyChange(String prop, Object oldValue, Object newValue) {
+
+        
+        if (SECONDE_PROP.equals(prop)) {
+            afficherHeure();
+        }
     }
 
     public void afficherHeure () {
