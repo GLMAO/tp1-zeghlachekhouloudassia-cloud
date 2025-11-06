8fe505fb7210cafbfc90d10a476990b0612d8675
Khouloud <zeghlachekhouloudassia@gmail.com>
2025-11-06 11:12:32 +0100

    Bug : la liste a été modifiée alors qu’elle était en cours de parcours

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -1,5 +1,8 @@
 package org.emp.gl.core.launcher;
 
+import java.util.Random;
+
+import org.emp.gl.clients.CompteARebours;
 import org.emp.gl.clients.Horloge ;
 import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
 import org.emp.gl.timer.service.TimerService;
@@ -22,14 +25,19 @@ public class App {
          
          TimerService timer = new DummyTimeServiceImpl();
     
-        Horloge h1 = new Horloge("Num 1", timer);
-        Horloge h2 = new Horloge("Num 2", timer);
-        Horloge h3 = new Horloge("Num3", timer);
-
+       // Horloge h1 = new Horloge("Num 1", timer);
+        //Horloge h2 = new Horloge("Num 2", timer);
+        //Horloge h3 = new Horloge("Num3", timer);
         
-    
-    }
+        new CompteARebours("C1", 5, timer);
 
+       
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int j=i+10;
+          new CompteARebours("C" + i, j, timer);
+        }
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
